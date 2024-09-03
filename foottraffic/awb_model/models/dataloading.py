c781b36dbe87be53e8ffbaf55dfc3453deb1c3d9
matthew.reda <matthew.reda@mediacom.com>
2024-09-03 16:45:04 -0400

    Sorted index for analytic dataframe

--- foottraffic/awb_model/models/dataloading.py
+++ foottraffic/awb_model/models/dataloading.py
@@ -238,6 +238,7 @@ class MFF(BaseModel):
         columns = [col for col in MFFCOLUMNS if not col in row_id+["VariableValue", "VariableName"]]
         df = df.pivot_table(index=[col for col in MFFCOLUMNS if col in row_id and not col == "Period"]+["Period"], columns=["VariableName"]+columns, values="VariableValue", aggfunc=aggfunc)
         df.columns = ["_".join(col) for col in df.columns]
+        df = df.sort_index()
         if indexed:
             return df
         return df.reset_index()
