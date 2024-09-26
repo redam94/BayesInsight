d38e23c6ffb5625795bec4df80030b79091c0f82
Matthew Reda <m.reda94@gmail.com>
2024-09-26 19:22:27 -0400

    Removed unused files and unusesd transforms

--- foottraffic/awb_model/transforms.py
+++ foottraffic/awb_model/transforms.py
@@ -4,12 +4,6 @@ import numpy as np
 def linear(x):
     return x
 
-def s_shaped(x, alpha: float, beta: float, index_to: float):
-    return (beta/(10**10)) ** (alpha ** (x/index_to*100))
-
-def s_origin(x, alpha: float, beta: float, index_to: float):
-    return (beta/(10**9)) ** (alpha ** (x/index_to*100)) - (beta/(10**9))
-
 def exp(x, alpha: float):
     return 1 - np.exp(-x/alpha)
 
